77f2bc8aaa671e98f8c72285a854f7472b94e73b
Richard Lichkus <rpl5018@gmail.com>
2014-08-12 20:17:53 -0700

    Initial Commit

---
+++ CollectionViewTransition/CKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CKAppDelegate.h
+//  CollectionViewTransition
+//
+//  Created by Richard Lichkus on 8/12/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewTransition/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewTransition
+//
+//  Created by Richard Lichkus on 8/12/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CKAppDelegate class]));
+    }
+}
